4cf727cb3b8df3e9529560639a2351e87e4acb01
Enrico Rossini <enrico.rossini.uk@live.com>
2021-02-03 13:15:44 +0000

    Update Main navigation with username or realname

---
+++ AdminLTEWithASPNETCore/Extensions/ClaimExtensions.cs
@@ -0,0 +1,82 @@
+﻿// ***********************************************************************
+// Assembly         : AdminLTEWithASPNETCore
+// Author           : enric
+// Blog             : 
+// Blog URL         : 
+// Created          : 02-03-2021
+//
+// Last Modified By : enric
+// Last Modified On : 02-03-2021
+// ***********************************************************************
+// <copyright file="ClaimExtensions.cs" company="AdminLTEWithASPNETCore">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AdminLTEWithASPNETCore.Extensions
+{
+    /// <summary>
+    /// Class ClaimExtensions.
+    /// </summary>
+    public static class ClaimExtensions
+    {
+        /// <summary>
+        /// Gets a claim from a list of claims
+        /// </summary>
+        /// <param name="claims"></param>
+        /// <param name="claimName"></param>
+        /// <returns></returns>
+        public static Claim GetClaim(this IEnumerable<Claim> claims, string claimName)
+        {
+            Claim rtn = null;
+
+            if (!string.IsNullOrEmpty(claimName))
+            {
+                rtn = claims.FirstOrDefault(x => x.Type == claimName);
+            }
+
+            return rtn;
+        }
+
+        /// <summary>
+        /// Gets the value of the requested claim if it exists
+        /// </summary>
+        /// <param name="claims"></param>
+        /// <param name="claimName"></param>
+        /// <returns></returns>
+        public static string GetClaimValue(this IEnumerable<Claim> claims, string claimName)
+        {
+            string rtn = null;
+
+            if (!string.IsNullOrEmpty(claimName))
+            {
+                Claim claim = GetClaim(claims, claimName);
+                if (claim != null)
+                    rtn = claim.Value;
+            }
+
+            return rtn;
+        }
+
+        /// <summary>
+        /// Updates a claim with a new value
+        /// </summary>
+        /// <param name="claims"></param>
+        /// <param name="claimName"></param>
+        /// <param name="newValue"></param>
+        public static void UpdateClaim(this List<Claim> claims, string claimName, string newValue)
+        {
+            Claim claim = claims.GetClaim(claimName);
+            if (claim != null)
+                claims.Remove(claim);
+
+            claims.Add(new Claim(claimName, newValue));
+        }
+    }
+}
---
+++ AdminLTEWithASPNETCore/Helpers/UIHelpers.cs
@@ -0,0 +1,48 @@
+﻿// ***********************************************************************
+// Assembly         : AdminLTEWithASPNETCore
+// Author           : enric
+// Blog             : 
+// Blog URL         : 
+// Created          : 02-03-2021
+//
+// Last Modified By : enric
+// Last Modified On : 02-03-2021
+// ***********************************************************************
+// <copyright file="UIHelpers.cs" company="AdminLTEWithASPNETCore">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using AdminLTEWithASPNETCore.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminLTEWithASPNETCore.Helpers
+{
+    /// <summary>
+    /// Class UIHelpers.
+    /// </summary>
+    public class UIHelpers
+    {
+        /// <summary>
+        /// Gets the real name for the current user from Active Directory
+        /// </summary>
+        /// <param name="claims"></param>
+        /// <returns></returns>
+        public static string GetRealName(IEnumerable<System.Security.Claims.Claim> claims)
+        {
+            string rtn = string.Empty;
+
+            if (claims != null)
+            {
+                rtn = claims.ToList().GetClaimValue("name");
+                if (string.IsNullOrEmpty(rtn))
+                    rtn = claims.ToList().GetClaimValue("email");
+            }
+
+            return rtn;
+        }
+    }
+}
